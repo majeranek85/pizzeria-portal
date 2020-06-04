734cd69f917aac1f9a92fda0e1968a32f4c1616e
Marta Majerowska <majeranek85@gmail.com>
2020-06-04 14:18:31 +0200

    Update

--- public/front/js/settings.js
+++ public/front/js/settings.js
@@ -116,7 +116,7 @@ export const settings = {
     tableIdAttribute: 'data-table',
   },
   db: {
-    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
+    url: '//localhost:3131/api',
     product: 'product',
     order: 'order',
     booking: 'booking',
--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React now!
+          Learn React right now!
         </a>
       </header>
     </div>
